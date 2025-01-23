f7aac31b532572b47282751e30580f47a36d5ea8
Omri Gal <omri.gal01@post.runi.ac.il>
2025-01-23 16:41:28 +0200

    ?

--- MyTester.java
+++ MyTester.java
@@ -1,24 +1,27 @@
 public class MyTester {
     public static void main (String[] args) {
-        MemorySpace memory = new MemorySpace(100);
-        int address = memory.malloc(20);
-        System.out.println(address);
-        System.out.println(memory.toString());
+        MemorySpace memorySpace = new MemorySpace(100);
+        int address1 = memorySpace.malloc(40);
+        int address2 = memorySpace.malloc(40);
+        int address3 = memorySpace.malloc(20);
+        System.out.println(address1 + " " + address2 + " " + address3);
+        System.out.println(memorySpace.toString());
     }
         /*
-    private boolean mallocTest1() {
+    private boolean mallocTest3() {
         MemorySpace memorySpace = new MemorySpace(100);
-        String expectedText = "(20 , 80) \n(0 , 20) ";
+        String expectedText = "\n(0 , 40) (40 , 40) (80 , 20) ";
         String expected = "true";
         String actual = "";
         try {
-            int address = memorySpace.malloc(20);
-            actual += (address == 0 && memorySpace.toString().equals(expectedText));
-            
+            int address1 = memorySpace.malloc(40);
+            int address2 = memorySpace.malloc(40);
+            int address3 = memorySpace.malloc(20);
+            actual += (address1 == 0 && address2 == 40 && address3 == 80 && memorySpace.toString().equals(expectedText));
         } catch (Exception e) {
             actual = TesterMessagesEnum.ERROR + e.getMessage();
         }
-        return this.tester.test("Malloc a block of memory", expected, actual);
+        return this.tester.test("Malloc 3 blocks of memory to fill all memory space", expected, actual);
     }
         */
 }
